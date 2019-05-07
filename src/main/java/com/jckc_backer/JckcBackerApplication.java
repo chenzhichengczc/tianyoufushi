f13faf0cdf5d69d9b3394b7d4717f915106e7ee2
chenzhichengczc <178642170@qq.com>
2019-05-07 15:29:42 +0800

    网页页面

---
+++ src/main/java/com/jckc_backer/JckcBackerApplication.java
@@ -0,0 +1,13 @@
+package com.jckc_backer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JckcBackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JckcBackerApplication.class, args);
+    }
+
+}
