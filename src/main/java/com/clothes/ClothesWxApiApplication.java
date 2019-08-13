99a2bcf4865ff75c44fd6e20af18be6a976ab897
chenzhichengczc <178642170@qq.com>
2019-08-13 16:07:25 +0800

    v1

---
+++ src/main/java/com/clothes/ClothesWxApiApplication.java
@@ -0,0 +1,41 @@
+package com.clothes;
+
+
+import com.alibaba.fastjson.JSON;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.List;
+
+
+/**
+ * @author ：fenghuang
+ * @date ：Created in 2019/8/13 15:48
+ * @description：
+ * @modified By：
+ * @version:
+ */
+
+@SpringBootApplication
+@MapperScan("com.clothes.modules.*.mapper")
+@SpringBootConfiguration
+@EnableTransactionManagement
+@EnableScheduling
+public class ClothesWxApiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ClothesWxApiApplication.class);
+    }
+
+
+
+
+}
+
